16b6b42e3bbaea0dcb4d490367c8b85bc77c747d
thiagoeu <araujo.thiago1051@gmail.com>
2023-10-12 23:04:32 -0300

    feat:: hero/CustomButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,13 @@
+"use client";
 import React from 'react'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
+  const handleScroll = () =>{
+
+  }
+  
   return (
     <div className='hero'>
       <div className="flex-1 pt-36 padding-x" > 
@@ -9,8 +16,21 @@ const Hero = () => {
         </h1>
         <p className='hero__subtitle'>
           Um novo conceito, mais pratico, mais economico ...
-        </p>
+        </p>  
+
+        <CustomButton 
+          title = "Explorar Carros"
+          containerStyles = "bg-primary-blue text-white rounded-full mt-10" 
+          handleClick = {handleScroll}
+        />
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image  src="/hero.png" alt ="hero" fill className ="object-contain"/>
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
+
     </div>
   )
 }
