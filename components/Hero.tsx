42599c800de830773d91ccfd7109f701e0364f52
thiagoeu <araujo.thiago1051@gmail.com>
2023-10-12 22:02:01 -0300

    feat:: Hero Component

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x" > 
+        <h1 className='hero__title'>
+          Encontre e reserve carros de maneira rapida e por um preço justo!
+        </h1>
+        <p className='hero__subtitle'>
+          Um novo conceito, mais pratico, mais economico ...
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
